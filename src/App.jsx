84169d7de9c0c2e4587d60161d3943a41b8c2560
KalSol12 <tenkirkaleab@gmail.com>
2024-05-19 16:16:31 -0700

    inline style for react

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 
 
 import Button from "./button/Button.jsx";
-import Button2 from "./button/Button2.jsx";
+
 
 function App() {
 
   return (
     <>
     <Button/>
-    <Button2/>
+    
     
     </>
       
