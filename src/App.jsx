10308cb5df9bb5adcd98978e5fd6664018fe1454
KalSol12 <tenkirkaleab@gmail.com>
2024-05-19 17:10:31 -0700

    works on props

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
 
 
-import Button from "./button/Button.jsx";
-
+import Student from "./Student";
 
 function App() {
 
   return (
     <>
-    <Button/>
-    
+    <Student name={"kaleab"} age={'25'} isStudent={true}/>
+    <Student name={90} age={'65'} isStudent={false}/>
+    <Student name={"teda"} age={23} isStudent={false}/>
     
     </>
       
     
   );
 }
+
 
 export default App
---
+++ src/Student.jsx
@@ -0,0 +1,18 @@
+import prototypes from 'prop-types'
+function Student(props) {
+  return(
+    <div className="student">
+          <p>Name:{props.name}</p>
+          <p>Age:{props.age}</p>
+          <p>Student:{props.isStudent ? "yes" :"no"}</p>
+
+    </div>
+  )
+  
+}
+Student.prototypes={
+  Name:prototypes.string,
+  Age:prototypes.number,
+  isStudent:prototypes.bool,
+}
+export default Student
